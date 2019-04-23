092c3660cafe23810704636558b5c4aa2a4b18db
aslampangestu <aslampangestu@gmail.com>
2019-04-23 23:45:40 +0700

    init react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      <p>Learn MERN</p>
+    </div>
+  );
+}
+
+export default App;
